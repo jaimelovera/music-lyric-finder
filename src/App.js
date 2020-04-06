31377396577b59a9558e041eec4fe3bc7f3e62cd
Jaime Lovera <lovera.jaime@gmail.com>
2020-04-06 01:53:39 -0700

    creating navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Navbar from './components/layout/Navbar.js'
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Navbar/>
     </div>
   );
 }
