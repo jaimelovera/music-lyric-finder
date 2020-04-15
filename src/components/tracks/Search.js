a316403ac9cd305c2b401f67f0b93dc3f53f60c6
Jaime Lovera <lovera.jaime@gmail.com>
2020-04-14 23:13:53 -0700

    added a dispatch call to context before the search api call to set track list to an empty array so that the track component rerenders as a loading animation while the api call returns with data. to let the user know the search has been requested.

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -12,15 +12,44 @@ import styles from './Search.module.css'
     this.setState({[e.target.name]: e.target.value})
   }
 
+  findTrack = (dispatch, e) => {
+    e.preventDefault()
+
+    dispatch({
+      type: 'SEARCH_TRACKS',
+      payload: []
+    })
+
+    axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`)
+    .then(res => {
+      if(res.data.message.header.available !== 0) {
+        dispatch({
+          type: 'SEARCH_TRACKS',
+          payload: res.data.message.body.track_list
+        })
+        this.setState({trackTitle: ''})
+      }
+      else {
+        alert("No results! Try again with a different song name.")
+      }
+    })
+    .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <Consumer>
         {value => {
+          const { dispatch } = value
           return (
             <div className={styles.searchContainer}>
               <h1>Search For A Song</h1>
               <p><i>Get the lyrics for any song</i></p>
-              <form className={styles.searchForm}>
+              <form 
+                onSubmit={this.findTrack.bind(this, dispatch)}
+                className={styles.searchForm}
+                autoComplete="off"
+              >
                 <input 
                   className={styles.searchInput}
                   type="text"
@@ -28,7 +57,7 @@ import styles from './Search.module.css'
                   name="trackTitle"
                   value={this.state.trackTitle}
                   onChange={this.onChange}
-                  autofocus
+                  autoFocus
                 />
                 <button className={styles.searchButton} type="submit">
                   Search Track Lyrics
